ddfe95d12a493bc74509700d2601bbf20e221366
azdd <160459052+AzdCreations@users.noreply.github.com>
2024-07-01 11:21:24 +0500

    m

--- backend/views.py
+++ backend/views.py
@@ -251,7 +251,7 @@ def index(request):
         return render(request, 'index-1.html', context)
     # Проверяем, принадлежит ли пользователь к группе "Менеджер"
     elif request.user.groups.filter(name='Бугалтер').exists():
-        return render(request, 'bugalter.html', context)
+        return render(request, '1-block/bugalter.html', context)
     
     else:
         # Если пользователь не входит ни в одну из этих групп
